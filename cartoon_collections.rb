3969caff2e8f81b664f5303b62f9cad1134cbc41
Amanda Rodgers <aarodgerswork@gmail.com>
2019-08-01 15:36:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,13 +11,18 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |calls| calls.capitalize + '!'}
 end
 
+<<<<<<< HEAD
 summon_captain_planet(planeteer_calls)
+=======
+
+>>>>>>> cf542c12896d3a43cd98793ff1bc9e7a00f5ebe1
 
 
 short_words = ["puff", "go", "two"]
 assorted_words = ["two", "go", "industrious", "bop"]
 
 def long_planeteer_calls(array)
+<<<<<<< HEAD
   array.reduce { |n| n.length > 4 }
 end
 
@@ -25,5 +30,15 @@ long_planeteer_calls(short_words)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+=======
+  array.any? { |n| n.length > 4 }
 end
+
+
+
+def find_the_cheese(array)
+>>>>>>> cf542c12896d3a43cd98793ff1bc9e7a00f5ebe1
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |n| cheese_types.include?(n)
+   end
+end   
